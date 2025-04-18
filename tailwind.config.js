1d555e6b6b5e06abc1a2d31c5584298825379600
Eniola Oke <okeeniola517@gmail.com>
2025-04-19 00:15:03 +0100

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      screens: {
+        
+     },
+      fontFamily:{
+        custom: ["Prosto-one"]
+      },
+      colors:{
+        "custom-color":"#8F0000"
+      }
+    },
+  },
+  plugins: [],
+}
+
